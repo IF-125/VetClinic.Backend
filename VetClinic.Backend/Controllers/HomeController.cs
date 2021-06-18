31c43cd807f8c1bb2dd38aac667abef53d6fd708
Serge <sanichace@gmail.com>
2021-06-18 15:03:47 +0300

    Add project files.

---
+++ VetClinic.Backend/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VetClinic.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok("Everyone gangsta till project initialization"); 
+        }
+    }
+}
