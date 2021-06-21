5de2c7a0b6a714a63e1727920cab440d09a00164
Vasyl Pavelko <kabool21@gmail.com>
2021-06-22 02:05:44 +0300

    Add Pet, PetImages, AnimalType models

---
+++ VetClinic.DAL/Configurations/PetEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace VetClinic.DAL.Configurations
+{
+    class PetEntityConfiguration: IEntityTypeConfiguration<Pet>
+    {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder
+                .HasKey(x => x.Id);
+
+            builder
+                .HasOne(x => x.AnimalType)
+                .WithMany(x => x.Pets)
+                .OnDelete(DeleteBehavior.Cascade);
+            
+            builder
+                .HasOne(x => x.Pet)
+                .WithMany(x => x.PetImages)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+    }
+}
