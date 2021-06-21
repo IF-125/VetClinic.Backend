22252749978545a88e20412f0592e65c982f9dd9
Serge <sanichace@gmail.com>
2021-06-21 23:12:52 +0300

    Created the following tables: Employees, Positions, EmployeePositions and Salaries. And also configuration for these tables has beed provided

---
+++ VetClinic.Core/Entities/Position.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace VetClinic.Core.Entities
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<EmployeePosition> EmployeePositions { get; set; }
+    }
+}
---
+++ VetClinic.Core/Entities/Salary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VetClinic.Core.Entities
+{
+    public class Salary
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Bonus { get; set; }
+        public int EmployeePositionId { get; set; }
+        public EmployeePosition EmployeePosition { get; set; }
+
+    }
+}
