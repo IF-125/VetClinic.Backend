d22cb76d4183cc7332962e5af85d15167faa8ea6
Vasyl Pavelko <kabool21@gmail.com>
2021-06-22 01:41:47 +0300

    Add Pet, PetImages, AnimalTypes models

---
+++ VetClinic.Core/Entities/AnimalType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic.Core.Entities
+{
+    class AnimalTypes
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+        public ICollection<Pet> Pets { get; set; }
+        
+    }
+}
---
+++ VetClinic.Core/Entities/PetImages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic.Core.Entities
+{
+    class PetImages
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+        public int PetId { get; set; }
+    }
+}
