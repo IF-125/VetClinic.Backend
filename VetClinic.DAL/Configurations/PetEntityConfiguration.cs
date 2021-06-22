3eb21c9a92e6e8ccb63dc209a5df85ea43925a6f
Vasyl Pavelko <kabool21@gmail.com>
2021-06-22 22:50:42 +0300

    Configure Pet model

--- VetClinic.DAL/Configurations/PetEntityConfiguration.cs
+++ VetClinic.DAL/Configurations/PetEntityConfiguration.cs
@@ -3,26 +3,31 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VetClinic.Core.Entities;
 
 
 namespace VetClinic.DAL.Configurations
 {
-    class PetEntityConfiguration: IEntityTypeConfiguration<Pet>
+    class PetEntityConfiguration : IEntityTypeConfiguration<Pet>
     {
         public void Configure(EntityTypeBuilder<Pet> builder)
         {
             builder
                 .HasKey(x => x.Id);
 
+            
+
             builder
                 .HasOne(x => x.AnimalType)
-                .WithMany(x => x.Pets)
+                .WithMany(x=>x.Pets)
                 .OnDelete(DeleteBehavior.Cascade);
-            
+
             builder
-                .HasOne(x => x.Pet)
-                .WithMany(x => x.PetImages)
+                .HasMany(x=>x.PetImages)
+                .WithOne(x=>x.Pet)
                 .OnDelete(DeleteBehavior.Cascade);
+
+            
 
         }
     }
