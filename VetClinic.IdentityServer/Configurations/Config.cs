67f093a6953cd8b72218ada6ab837831d993bb48
lubomir97 <lubomir_demchuk@i.ua>
2021-06-26 15:21:18 +0300

    base modification

---
+++ VetClinic.IdentityServer/Configurations/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VetClinic.IdentityServer.Configurations
+{
+    public static class Config
+    {
+        public static IEnumerable<User> GetUsers()
+        {
+            
+        }
+    }
+}
