cf138ca637cd282fed6ae909191ad9e4e85945d9
Serge <sanichace@gmail.com>
2021-06-27 23:44:02 +0300

    created and implemented base repository (also removed entities for this branch)

--- VetClinic.Core/Entities/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VetClinic.Core.Entities
-{
-    public class Employee : User
-    {
-        public string Address { get; set; }
-        public int? EmployeePositionId { get; set; }
-        public EmployeePosition EmployeePosition { get; set; }
-    }
-}
