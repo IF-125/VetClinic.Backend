9dce765f9eb71507f69df8f520b4f03ce6a794e9
Stanislav Kudriavtsev <equilibrium42@protonmail.com>
2021-07-11 18:56:12 +0300

    Added ProcedureController and ProcedureService.

---
+++ VetClinic.DAL/Repositories/ProcedureRepository.cs
@@ -0,0 +1,15 @@
+﻿using VetClinic.Core.Entities;
+using VetClinic.Core.Interfaces.Repositories;
+using VetClinic.DAL.Context;
+using VetClinic.DAL.Repositories.Base;
+
+namespace VetClinic.DAL.Repositories
+{
+    public class ProcedureRepository : Repository<Procedure>, IProcedureRepository
+    {
+        public ProcedureRepository(VetClinicDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ VetClinic.WebApi/ViewModels/ProcedureViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VetClinic.WebApi.ViewModels
+{
+    public class ProcedureViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Duration { get; set; }
+        public decimal Price { get; set; }
+    }
+}
