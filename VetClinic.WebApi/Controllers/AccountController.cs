612a0afaa73a08d4516858a38a454e17ae524f43
lubomir97 <lubomir_demchuk@i.ua>
2021-08-05 15:23:57 +0300

    Added AccountController with Register & Login

---
+++ VetClinic.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VetClinic.Core.Entities;
+using VetClinic.WebApi.ViewModels;
+
+namespace VetClinic.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+  
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+           
+            User user = new User { Email = model.Email, UserName = model.Email, FirstName = model.FirstName, LastName = model.LastName };
+                
+            var result = await _userManager.CreateAsync(user, model.Password);
+            await _signInManager.SignInAsync(user, false);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
+
+            await _userManager.AddClaimAsync(user, new System.Security.Claims.Claim("Role", "Client"));
+            return Ok();
+        }
+
+        [Route("Login")]
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            var res = returnUrl;
+            return Content(res);
+        }
+        
+        [HttpPost("validate")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            
+            var result = await _signInManager.PasswordSignInAsync(model.Email,
+                                                                      model.Password,
+                                                                      model.RememberMe,
+                                                                      false);
+            if (result.Succeeded)
+            {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Wrong login or password");
+            }
+            
+            return Ok(model);
+        }
+
+        [HttpPost("Logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("api/Home");
+        }
+    }
+}
---
+++ VetClinic.WebApi/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace VetClinic.WebApi.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords doesn't match!")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        public string PasswordConfirm { get; set; }
+    }
+}
