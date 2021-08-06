630f636c8ea2c7d3a6ed0c6b85e8b3186598a3d5
lubomir97 <lubomir_demchuk@i.ua>
2021-08-06 11:07:24 +0300

    Modified getting toke from identityserver

--- VetClinic.WebApi/Controllers/IdentityController.cs
+++ VetClinic.WebApi/Controllers/IdentityController.cs
@@ -1,9 +1,6 @@
 ﻿using IdentityModel.Client;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace VetClinic.WebApi.Controllers
@@ -21,20 +18,6 @@ namespace VetClinic.WebApi.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            string accessToken = await HttpContext.GetTokenAsync("access_token");
-            string refreshToken = await HttpContext.GetTokenAsync("refresh_token");
-
-            return Content($"Current user: <span id=\"UserIdentityName\">{User.Identity.Name ?? "anonymous"}</span><br/>" +
-                $"<div>Access token: {accessToken}</div><br/>" +
-                $"<div>Refresh token: {refreshToken}</div><br/>"
-                , "text/html");
-        }
-
-
-        //[Route("/")]
         [HttpGet("callapi")]
         public async Task<IActionResult> CallApi()
         {
@@ -65,21 +48,6 @@ namespace VetClinic.WebApi.Controllers
                 access_token = tokenResponse.AccessToken,
                 message = content
             });
-
-
-            //string accessToken = await HttpContext.GetTokenAsync("access_token");
-            //var request = new HttpRequestMessage(HttpMethod.Post, "https://localhost:5101/connect/token");
-
-
-            //request.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
-            //HttpResponseMessage response = await HttpClient.SendAsync(request);
-
-            //if (response.StatusCode != HttpStatusCode.OK)
-            //{
-            //    return Content(response.ToString());
-            //}
-
-            //return Content($"{await response.Content.ReadAsStringAsync()}");
         }
     }
 }
