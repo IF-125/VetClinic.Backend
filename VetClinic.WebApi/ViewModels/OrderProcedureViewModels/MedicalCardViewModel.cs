a13fe65a844119e9d970eba0ed68ec688b0b76ab
Serge <sanichace@gmail.com>
2021-08-14 15:50:27 +0300

    removed Count property from MedicalCardViewModel

--- VetClinic.WebApi/ViewModels/OrderProcedureViewModels/MedicalCardViewModel.cs
+++ VetClinic.WebApi/ViewModels/OrderProcedureViewModels/MedicalCardViewModel.cs
@@ -3,7 +3,6 @@
     public class MedicalCardViewModel
     {
         public int Id { get; set; }
-        public int Count { get; set; }
         public string TotalDuration { get; set; }
         public string Conclusion { get; set; }
         public string Details { get; set; }
