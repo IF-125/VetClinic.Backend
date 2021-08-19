4977f17d4126e9a22bd32a5f82b17fb74e73d8f8
Serge <sanichace@gmail.com>
2021-08-19 13:33:50 +0300

    moved AuthResponseViewModel into specific folder

--- VetClinic.WebApi/ViewModels/AuthViewModels/AuthResponseViewModel.cs
+++ VetClinic.WebApi/ViewModels/AuthViewModels/AuthResponseViewModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace VetClinic.WebApi.ViewModels
+namespace VetClinic.WebApi.ViewModels.AuthViewModels
 {
     public class AuthResponseViewModel
     {
