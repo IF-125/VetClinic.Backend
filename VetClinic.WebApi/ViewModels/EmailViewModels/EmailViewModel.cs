b678c9a1f145c95ab24f3469a6329906f1cb577e
Stanislav Kudriavtsev <equilibrium42@protonmail.com>
2021-08-20 16:31:59 +0300

    Added Email Service

---
+++ VetClinic.WebApi/ViewModels/EmailViewModels/EmailViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VetClinic.WebApi.ViewModels
+{
+    public class EmailViewModel
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
