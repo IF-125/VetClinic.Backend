03a82ef222ef75fda1f6e23e5ae7d5de993e77b8
Serge <sanichace@gmail.com>
2021-08-26 21:53:16 +0300

    removed unnecessary usings from AuthResponseViewModel

--- VetClinic.WebApi/ViewModels/AuthViewModels/AuthResponseViewModel.cs
+++ VetClinic.WebApi/ViewModels/AuthViewModels/AuthResponseViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace VetClinic.WebApi.ViewModels.AuthViewModels
+﻿namespace VetClinic.WebApi.ViewModels.AuthViewModels
 {
     public class AuthResponseViewModel
     {
